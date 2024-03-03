7772444a3fe3525ade1a49d2ce38743d2156c8a7
Yusee480 <adamuyusuf480@gmail.com>
2024-03-03 01:20:04 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,58 @@
+// let calc= 98+58;
+// document.write(calc);
+
+// let human ={
+//     color:'red',
+//     width: 100,
+//     hieght: 'tell',
+//     age: 10,
+// }
+// document.write(human.color, human.hieght, human.age);
+
+
+// let text= 'musa';
+// if( text =='yusee'){
+//     alert('wellcome'+ text)
+// }else if(text == 'sani'){
+//     alert('welcome'+ text)
+// }
+// else{
+//     alert('your not part of computer sceince')
+// }
+
+// var myName='Yusee';
+// if (myName =='Yusee'){
+//     prompt('enter your name')
+// }else{
+//     alert('Hello world')
+// } 
+// console.log(myName)
+
+// let human={
+//     color:'red',
+//     width: 'null',
+//     height: 'treu',
+// }
+// console.log(human.color, human.width, human.height)
+// document.write(human.color, human.width, human.height)
+// var massage='hello world';
+// console.log(massage)
+// let name='Yusee';
+// var age=30;
+// let isStudent =true;
+// let number=[1,1,2,3,4,5,6];
+// var person={
+//     name:'Yusee',
+//     age= 30,
+// }
+// 
+var num=10;
+if (num <=0){
+    document.write('positive number')
+}else{
+    document.write('nagetive number')
+
+}
+for(let i=0; i<5; i++){
+    document.write(i)
+}
